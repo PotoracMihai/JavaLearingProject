7ad7f26cbf79b6e138639200c864ee19ea70bb3d
Mihai <potoracmihai74@gmail.com>
2024-11-14 16:20:09 +0200

    Add Different Methods, Overloading

---
+++ src/main/java/md/maib/methodstask/ExecutionService.java
@@ -0,0 +1,51 @@
+package md.maib.methodstask;
+
+public class ExecutionService {
+    public static void main(String[] args) {
+        int randomInt = DataGeneratorUtil.getRandomInt(150, 350);
+        System.out.println("Variabila randomInt: " + randomInt);
+
+        int randomInt1 = DataGeneratorUtil.getRandomInt(150);
+        System.out.println("Variabila randomInt1: " + randomInt1);
+
+        double randomDouble = DataGeneratorUtil.getRandomDouble(1000.0);
+        System.out.println("Variabila radomDouble: " + randomDouble);
+
+        double randomDouble1 = DataGeneratorUtil.getRandomDouble(-1000.0);
+        System.out.println("Variabila randomDouble1: " + randomDouble1);
+
+        System.out.println("Random boolean: " + DataGeneratorUtil.getRandomBoolean());
+
+        System.out.println("Random email 1: " + DataGeneratorUtil.getRandomEmail());
+        System.out.println("Random email 2: " + DataGeneratorUtil.getRandomEmail());
+
+        System.out.println("Random Valid String de X chars: " + DataGeneratorUtil.generateValidRandomString(256));
+        System.out.println("Random Invalid String de X chars: " + DataGeneratorUtil.generateInvalidRandomString(257));
+        System.out.println("Random Invalid String de X chars: " + DataGeneratorUtil.generateInvalidRandomString(5));
+
+        Customer ioncustomer = new Customer("Ion Lungu" , "Ion.lungu@gmail.com", 26);
+
+        System.out.println("Nume obiect Ion: " + ioncustomer.getName());
+        System.out.println("Email obiect Ion: " + ioncustomer.getEmail());
+        System.out.println("Varsta obiect Ion: " + ioncustomer.getAge());
+
+        ioncustomer.setName("Ion Borta");
+        ioncustomer.setEmail("IonBorta@gmail.com");
+        ioncustomer.setAge(29);
+
+        System.out.println("Nume obiect Ion: " + ioncustomer.getName());
+        System.out.println("Email obiect Ion: " + ioncustomer.getEmail());
+        System.out.println("Varsta obiect Ion: " + ioncustomer.getAge());
+
+        Customer randomDataCustomer = new Customer(DataGeneratorUtil.generateValidRandomString(12),
+                    DataGeneratorUtil.getRandomEmail(), DataGeneratorUtil.getRandomInt(127));
+
+        System.out.println(randomDataCustomer.toString());
+
+        randomDataCustomer.setName(DataGeneratorUtil.generateValidRandomString(6));
+        randomDataCustomer.setAge(DataGeneratorUtil.getRandomInt(18,64));
+        randomDataCustomer.setEmail(DataGeneratorUtil.getRandomEmail());
+
+        System.out.println(randomDataCustomer.toString());
+    }
+}
